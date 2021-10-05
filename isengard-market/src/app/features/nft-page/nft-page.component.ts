cfde237eaa45a2d466b2b5e360b6d03f2bf984a7
marius <marius.brinzaru@omnisourcetech.com>
2021-10-05 19:29:54 +0300

    NFT page static design 45%

---
+++ isengard-market/src/app/features/nft-page/nft-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-page',
+  templateUrl: './nft-page.component.html',
+  styleUrls: ['./nft-page.component.scss']
+})
+export class NFTPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
