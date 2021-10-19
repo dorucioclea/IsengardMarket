6612a098c16a7af0eb006e82fb6345983c271192
Razvan <merdescu.razvan@gmail.com>
2021-10-20 01:17:55 +0300

    Remove unused class, to be added later if needed.

--- src/app/features/nft-page/nft-page.module.ts
+++ src/app/features/nft-page/nft-page.module.ts
@@ -8,12 +8,6 @@ import { RouterModule } from "@angular/router";
 import { MatDialogModule } from '@angular/material/dialog';
 import { NftSellDialog } from "./dialogs/nft-sell-dialog.component";
 import { FormsModule } from '@angular/forms';
-import { NftMintedNotForSaleComponent } from "./nft-minted-not-for-sale/nft-minted-not-for-sale.component";
-import { NftMintedForSaleComponent } from "./nft-minted-for-sale/nft-minted-for-sale.component";
-import { NftNotMintedForSaleComponent } from "./nft-not-minted-for-sale/nft-not-minted-for-sale.component";
-import { NftNotMintedNotForSaleComponent } from "./nft-not-minted-not-for-sale/nft-not-minted-not-for-sale.component";
-import { NftNotMintedAuctionComponent } from "./nft-not-minted-auction/nft-not-minted-auction.component";
-import { NftMintedAuctionComponent } from "./nft-minted-auction/nft-minted-auction.component";
 import { NftAuctionDialog } from "./dialogs/nft-auction-dialog.component";
 import { BidAuctionDialog } from "./dialogs/bid-auction-dialog.component";
 
@@ -30,13 +24,7 @@ import { BidAuctionDialog } from "./dialogs/bid-auction-dialog.component";
   declarations: [NFTPageComponent,
     NftSellDialog,
     NftAuctionDialog,
-    BidAuctionDialog,
-    NftMintedNotForSaleComponent,
-    NftMintedForSaleComponent,
-    NftNotMintedForSaleComponent,
-    NftNotMintedNotForSaleComponent,
-    NftNotMintedAuctionComponent,
-    NftMintedAuctionComponent],
+    BidAuctionDialog],
   exports: [NFTPageComponent]
 })
 
