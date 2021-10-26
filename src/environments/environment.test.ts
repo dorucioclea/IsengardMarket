8956615dd2d466a5c444aed6d3a4935379b440e5
Razvan <merdescu.razvan@gmail.com>
2021-10-26 17:20:36 +0300

    Update dev env

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   maintenance: false,
-  marketUrl: 'https://isengard-20253.web.app',
+  marketUrl: 'https://dev.isengard.market',
   backendUri: 'https://isengardappapidev.azurewebsites.net',
   elrondApiUri:'https://devnet-api.elrond.com',
   contractAddress: 'erd1qqqqqqqqqqqqqpgqrzp86rsuugkhcr7ftdeeksupe9frjhlelwfqmunyzf',
